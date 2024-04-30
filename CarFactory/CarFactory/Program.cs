5cc76c933ca812bb7758a0f0abe0447a38d3a6e8
1mumble1 <mrt47@yandex.ru>
2024-05-01 00:03:04 +0300

    Добавлен namespace, убран лишний else, улучшено взаимодействие с юзером

--- CarFactory/CarFactory/Program.cs
+++ CarFactory/CarFactory/Program.cs
@@ -1,12 +1,13 @@
 ﻿using CarFactory.Models.Cars;
 
+namespace CarFactory;
 public class Program
 {
     private const string Yes = "y";
     public static void Main()
     {
         Console.WriteLine("Добро пожаловать на фабрику машин!");
-        
+
         while (true)
         {
             Console.WriteLine("Давайте соберем для вас машину");
@@ -15,18 +16,17 @@ public class Program
 
             Console.WriteLine("Характеристики вашей машины:");
             Console.WriteLine(car.ToString());
-            Console.WriteLine("Хотите собрать еще одну машину? (Press Enter to exit, y to confirm)");
+
+            Console.WriteLine("Хотите собрать еще одну машину? (введите y для потверждения и любую клавишу для завершения)");
             string? confirmationInput = Console.ReadLine();
             Console.Clear();
             if (confirmationInput == Yes)
             {
                 continue;
             }
-            else
-            {
-                Console.WriteLine("До свидания!");
-                break;
-            }
+
+            Console.WriteLine("До свидания!");
+            break;
         }
     }
 }
